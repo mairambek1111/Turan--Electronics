bd96474423d6b49dca3cfbdb07e83f1f81b9fc95
sanjar <san4o.myrzakmatov@gmail.com>
2024-03-02 18:31:56 +0600

    newPost

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,11 @@
 import Footer from "../../components/footer/footer";
 import Header from "../../components/header/header";
+import NewPostupleniya from "../../components/newPostupleniya/index.jsx";
 function Home() {
   return (
     <>
       <Header />
+        <NewPostupleniya/>
       <Footer />
     </>
   );
