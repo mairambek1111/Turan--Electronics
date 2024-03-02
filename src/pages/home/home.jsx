683e6fbc98b9b6cb2ce2d1d703c32102ad3c4fd0
Ayana <aqyanatabaldieva@gmail.com>
2024-03-02 18:24:58 +0600

    finished hero CardHero

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,16 @@
+import Cardhero from "../../components/cardHero/cardhero";
 import Footer from "../../components/footer/footer";
 import Header from "../../components/header/header";
+import Hero from "../../components/hero/hero";
 function Home() {
-  return (
-    <>
-      <Header />
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Hero />
+            <Cardhero />
+            <Footer />
+        </>
+    );
 }
 
 export default Home;
