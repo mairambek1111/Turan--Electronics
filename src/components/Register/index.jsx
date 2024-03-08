7b434bb2986e33a4302edd8ce4687e8edce5c9d4
sanjar <san4o.myrzakmatov@gmail.com>
2024-03-09 01:09:55 +0600

    avtAdap

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,15 +1,15 @@
-import React, {useState} from "react";
+import {useState} from "react";
 import "./Register.scss";
 import { Link } from "react-router-dom";
 import registerImage from "../../assets/register.png";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import {FaEye, FaEyeSlash, FaFacebook} from "react-icons/fa";
 import { FaTelegram } from "react-icons/fa";
-import Enter from "./Enter";
 
 export default function Register() {
-  const [showEnter, setShowEnter] = React.useState(false);
+  // const [showEnter, setShowEnter] = React.useState(false);
   const [eye,setEye] = useState('password')
+  const [active,setActive] = useState(false)
 
   const handleEye = ()=>{
     if(eye === 'password'){
@@ -18,11 +18,45 @@ export default function Register() {
       setEye('password')
     }
   }
+function iValue(){
+  const iName = document.querySelector('.iName')
+  const iPass = document.querySelector('.iPass')
+  const iPass2 = document.querySelector('.iPass2')
+  const iEmail = document.querySelector('.iEmail')
+  if(iName.value.trim() === ''){
+    iName.style.border = '1px solid red'
+  }else {
+    iName.style.border = '1px solid rgba(255, 255, 255, 1)'
+  }
+  if(iPass.value.trim() === ''){
+    iPass.style.border = '1px solid red'
+  }else {
+    iPass.style.border = '1px solid rgba(255, 255, 255, 1)'
+  }
+  if(iPass2.value.trim() === ''){
+    iPass2.style.border = '1px solid red'
+  }else {
+    iPass2.style.border = '1px solid rgba(255, 255, 255, 1)'
+  }
+  if(iEmail.value.trim() === ''){
+    iEmail.style.border = '1px solid red'
+  }else {
+    iEmail.style.border = '1px solid rgba(255, 255, 255, 1)'
+  }
+}
+  const btnActive = ()=>{
+    setActive(true)
+    iValue()
+  }
+  const validation = ()=>{
+    if(active === true){
+      iValue()
+    }
+  }
+
 
   return (
     <>
-      {showEnter && <Enter />}
-      {showEnter === false && (
         <div id="register">
           <div className="container">
             <h1>Регистрация</h1>
@@ -35,22 +69,24 @@ export default function Register() {
               </div>
               <div className="register__inputs">
                 <input
+                    className='iName'
                   type="text"
                   placeholder="Имя"
+                    onInput={validation}
                 />
                 <div className="register__inputs--pass">
-                  <input type={eye} placeholder="Пароль"/>
+                  <input className='iPass' type={eye} placeholder="Пароль" onInput={validation}/>
                   {
                     eye === 'password' ? <FaEyeSlash onClick={handleEye} className='passEye'/> :<FaEye onClick={handleEye} className='passEye'/>
                   }
                 </div>
                 <div className="register__inputs--pass">
-                <input type={eye} placeholder="Потвердить пароль" />
+                <input className='iPass2' type={eye} placeholder="Потвердить пароль" onInput={validation}/>
                   {
                     eye === 'password' ? <FaEyeSlash onClick={handleEye} className='passEye'/> :<FaEye onClick={handleEye} className='passEye'/>
                   }
                 </div>
-                <input type="text" placeholder="Email или телефон ..." />
+                <input className='iEmail' type="text" placeholder="Email или телефон ..." onInput={validation}/>
                 <div className="register__inputs--check">
                   <input type="checkbox" id="check" />
                   <label htmlFor="check">
@@ -62,11 +98,11 @@ export default function Register() {
                 </div>
               </div>
               <div className="register__button">
-                <button>Регистрация</button>
+                <button onClick={btnActive}>Регистрация</button>
               </div>
               <div className="register__links">
                 <h2>Уже регистрировались? </h2>
-                <Link onClick={() => setShowEnter(true)}>Войти</Link>
+                <Link>Войти</Link>
               </div>
               <img src={registerImage} alt="" />
               <div className="register__network">
@@ -95,7 +131,6 @@ export default function Register() {
             </div>
           </div>
         </div>
-      )}
     </>
   );
 }
