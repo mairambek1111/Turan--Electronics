19e9e48f5c9ca2a2218205beda3572bd1d59f03c
mairambek <mairambekjoldoshbekov30@gmail.com>
2024-03-08 16:52:54 +0600

    fixed bag master

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,21 +3,22 @@ import Footer from "../../components/footer/footer";
 import Header from "../../components/header/header";
 import NewPostupleniya from "../../components/newPostupleniya/index.jsx";
 import Hero from "../../components/hero/hero";
-import Recomendation from "../../components/recomendation/Recomendation.jsx";
 import SwipeBasket from "../../components/swipeBasket/swipeBasket.jsx";
-import Navbtn from "../../components/navbtn/navbtn.jsx";
 import Brends from "../../components/Brends/Brends.jsx";
-import Register from "../../components/Register/index.jsx";
-import Authorization from "../../components/Authorization/index.jsx";
+import Navbtn from "../../components/navbtn/navbtn.jsx";
+import Recomendation from "../../components/recomendation/Recomendation.jsx";
+
 function Home() {
-   
   return (
     <>
       <Header />
+      <Navbtn />
       <Hero />
       <Cardhero />
       <SwipeBasket />
       <NewPostupleniya />
+      <Recomendation />
+      <Brends />
       <Footer />
     </>
   );
