bee7b01919e1fe82f0efce211281c009caa62bb5
sanjar <san4o.myrzakmatov@gmail.com>
2024-03-20 00:28:00 +0600

    filterBacks

--- src/components/newPostupleniya/newPostupleniyaCard.jsx
+++ src/components/newPostupleniya/newPostupleniyaCard.jsx
@@ -8,6 +8,7 @@ import {FaBagShopping} from "react-icons/fa6";
 
 
 const NewPostupleniyaCard = ({el}) => {
+    // qwertyui
     const [heart, setHeart] = useState(false)
     const [bag, setBag] = useState(false)
     const nav = useNavigate()
