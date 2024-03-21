21e3f7ba4a839119919b77598229183ae90decab
sanjar <san4o.myrzakmatov@gmail.com>
2024-03-21 22:17:49 +0600

    acses

--- src/components/swipeBasket/swipeBasket.jsx
+++ src/components/swipeBasket/swipeBasket.jsx
@@ -1,12 +1,22 @@
-import { FaArrowLeft } from "react-icons/fa";
+import {FaArrowLeft} from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 import "./swipeBasket.css";
-import basketIcon from "../../assets/basket.png";
-import img from "../../assets/image.png";
-import { RiStarSFill } from "react-icons/ri";
 import Slider from "react-slick/lib/slider";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import SwipeCard from "./swipeCard.jsx";
 
 function SwipeBasket() {
+  const [acses,setAcses] = useState([])
+  const getAcses = async () =>{
+    const res = await axios(`https://oceanbackend.pythonanywhere.com/product/`)
+    const {data} = await res
+    setAcses(data.filter(el => el.brand === 'Accessories'))
+  }
+  useEffect(() => {
+    getAcses()
+  }, []);
+
   const settings = {
     infinite: true,
     slidesToShow: 4,
@@ -56,120 +66,9 @@ function SwipeBasket() {
           </div>
         </div>
         <Slider {...settings}>
-          <div className="card" data-aos="zoom-in-up" data-aos-duration="1100">
-            <div className="stars">
-              <RiStarSFill color="rgba(255, 115, 0, 1)" fontSize={20} />
-              <RiStarSFill color="rgba(255, 115, 0, 1)" fontSize={20} />
-
-              <RiStarSFill fontSize={20} />
-              <RiStarSFill fontSize={20} />
-              <RiStarSFill fontSize={20} />
-            </div>
-
-            <img src={img} alt="" className="basketImage" />
-
-            <h3 className="basketTitle">Зарядное устройство</h3>
-            <p className="basketPrice">3500 сом</p>
-            <div className="basket">
-              <button className="btnBasket">Быстрый заказ</button>
-              <img src={basketIcon} alt="" className="basketIcon" />
-            </div>
-          </div>
-          <div className="card" data-aos="zoom-in-up" data-aos-duration="1100">
-            <div className="stars">
-              <RiStarSFill color="rgba(255, 115, 0, 1)" fontSize={20} />
-              <RiStarSFill color="rgba(255, 115, 0, 1)" fontSize={20} />
-
-              <RiStarSFill fontSize={20} />
-              <RiStarSFill fontSize={20} />
-              <RiStarSFill fontSize={20} />
-            </div>
-
-            <img src={img} alt="" className="basketImage" />
-
-            <h3 className="basketTitle">Зарядное устройство</h3>
-            <p className="basketPrice">3500 сом</p>
-            <div className="basket">
-              <button className="btnBasket">Быстрый заказ</button>
-              <img src={basketIcon} alt="" className="basketIcon" />
-            </div>
-          </div>{" "}
-          <div className="card" data-aos="zoom-in-up" data-aos-duration="1100">
-            <div className="stars">
-              <RiStarSFill color="rgba(255, 115, 0, 1)" fontSize={20} />
-              <RiStarSFill color="rgba(255, 115, 0, 1)" fontSize={20} />
-
-              <RiStarSFill fontSize={20} />
-              <RiStarSFill fontSize={20} />
-              <RiStarSFill fontSize={20} />
-            </div>
-
-            <img src={img} alt="" className="basketImage" />
-
-            <h3 className="basketTitle">Зарядное устройство</h3>
-            <p className="basketPrice">3500 сом</p>
-            <div className="basket">
-              <button className="btnBasket">Быстрый заказ</button>
-              <img src={basketIcon} alt="" className="basketIcon" />
-            </div>
-          </div>
-          <div className="card" data-aos="zoom-in-up" data-aos-duration="1100">
-            <div className="stars">
-              <RiStarSFill color="rgba(255, 115, 0, 1)" fontSize={20} />
-              <RiStarSFill color="rgba(255, 115, 0, 1)" fontSize={20} />
-
-              <RiStarSFill fontSize={20} />
-              <RiStarSFill fontSize={20} />
-              <RiStarSFill fontSize={20} />
-            </div>
-
-            <img src={img} alt="" className="basketImage" />
-
-            <h3 className="basketTitle">Зарядное устройство</h3>
-            <p className="basketPrice">3500 сом</p>
-            <div className="basket">
-              <button className="btnBasket">Быстрый заказ</button>
-              <img src={basketIcon} alt="" className="basketIcon" />
-            </div>
-          </div>
-          <div className="card" data-aos="zoom-in-up" data-aos-duration="1100">
-            <div className="stars">
-              <RiStarSFill color="rgba(255, 115, 0, 1)" fontSize={20} />
-              <RiStarSFill color="rgba(255, 115, 0, 1)" fontSize={20} />
-
-              <RiStarSFill fontSize={20} />
-              <RiStarSFill fontSize={20} />
-              <RiStarSFill fontSize={20} />
-            </div>
-
-            <img src={img} alt="" className="basketImage" />
-
-            <h3 className="basketTitle">Зарядное устройство</h3>
-            <p className="basketPrice">3500 сом</p>
-            <div className="basket">
-              <button className="btnBasket">Быстрый заказ</button>
-              <img src={basketIcon} alt="" className="basketIcon" />
-            </div>
-          </div>
-          <div className="card" data-aos="zoom-in-up" data-aos-duration="1100">
-            <div className="stars">
-              <RiStarSFill color="rgba(255, 115, 0, 1)" fontSize={20} />
-              <RiStarSFill color="rgba(255, 115, 0, 1)" fontSize={20} />
-
-              <RiStarSFill fontSize={20} />
-              <RiStarSFill fontSize={20} />
-              <RiStarSFill fontSize={20} />
-            </div>
-
-            <img src={img} alt="" className="basketImage" />
-
-            <h3 className="basketTitle">Зарядное устройство</h3>
-            <p className="basketPrice">3500 сом</p>
-            <div className="basket">
-              <button className="btnBasket">Быстрый заказ</button>
-              <img src={basketIcon} alt="" className="basketIcon" />
-            </div>
-          </div>
+          {
+            acses.map(el => <SwipeCard key={el.id} el={el}/>)
+          }
         </Slider>
       </div>
     </>
---
+++ src/components/swipeBasket/swipeCard.jsx
@@ -0,0 +1,53 @@
+import {Link, useNavigate} from "react-router-dom";
+import {TbShoppingBag, TbShoppingBagCheck} from "react-icons/tb";
+import axios from "axios";
+import {useState} from "react";
+import {FaStar} from "react-icons/fa";
+
+const SwipeCard = ({el}) => {
+    const [bag, setBag] = useState(false)
+    const nav = useNavigate()
+    const addBasket = async () => {
+        const data = {
+            product: el.id,
+            user: 1,
+            summ_products: el.price
+        }
+        const response = await axios.get(`https://oceanbackend.pythonanywhere.com/basket`)
+        const every = response.data.map((el)=> el.product)
+        const existingItem = every.find(item => item.id === el.id);
+        if (existingItem) {
+            nav('/headerBasket')
+        } else {
+            await axios.post(`https://oceanbackend.pythonanywhere.com/basket_post/`,data);
+            setBag(true)
+        }
+    }
+    const starsCount = el.stars
+    const maxStars = 5
+    const stars = [];
+    for (let i = 0; i < maxStars; i++) {
+        stars.push(<FaStar key={i} className={i < starsCount ? "starsYellow" : "starsNone"} />);
+    }
+    return (
+        <div className="card" data-aos="zoom-in-up" data-aos-duration="1100">
+            <div className="stars">
+                {stars}
+            </div>
+            <img src={el.first_photo} alt="no img" className="basketImage"/>
+            <h3 className="basketTitle">{el.name}</h3>
+            <p className="basketPrice">{el.price} сом</p>
+            <div className="basket">
+                <Link to={`/product/${el.id}`}>
+                    <button className="btnBasket">Быстрый заказ</button>
+                </Link>
+                {
+                    bag ? <TbShoppingBagCheck onClick={() => nav('/headerBasket')} className="basketIcon"/> :
+                        <TbShoppingBag onClick={() => addBasket(el)} className="basketIcon"/>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default SwipeCard;
