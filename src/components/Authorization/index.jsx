6e3187c45051ae656e40f2ff35688f97b060bf65
mairambek <mairambekjoldoshbekov30@gmail.com>
2024-03-21 16:38:40 +0600

    finished authorization

--- src/components/Authorization/index.jsx
+++ src/components/Authorization/index.jsx
@@ -1,136 +1,3 @@
-// import "./Authorization.scss";
-// import { Link, useNavigate } from "react-router-dom";
-// import registerImage from "../../assets/register.png";
-// import { AiFillGoogleCircle } from "react-icons/ai";
-// import { FaEye, FaEyeSlash, FaFacebook } from "react-icons/fa";
-// import { FaTelegram } from "react-icons/fa";
-// import { useState } from "react";
-// import Register from "../Register";
-
-// const Authorization = () => {
-//   const [active, setActive] = useState(false);
-//   const [eye, setEye] = useState("password");
-//   const [showRegister, setShowRegister] = useState(false);
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const handleEye = () => {
-//     if (eye === "password") {
-//       setEye("text");
-//     } else {
-//       setEye("password");
-//     }
-//   };
-
-//   function iValue() {
-//     const iPass = document.querySelector(".iPass");
-//     const iEmail = document.querySelector(".iEmail");
-//     if (iPass.value.trim() === "") {
-//       iPass.style.border = "1px solid red";
-//     } else {
-//       iPass.style.border = "1px solid #a7c957";
-//     }
-//     if (iEmail.value.trim() === "") {
-//       iEmail.style.border = "1px solid red";
-//     } else {
-//       iEmail.style.border = "1px solid #a7c957";
-//     }
-//   }
-
-//   function funcs() {
-//     btnActive();
-//   }
-//   const btnActive = () => {
-//     setActive(true);
-//     iValue();
-//   };
-//   const validation = () => {
-//     if (active === true) {
-//       iValue();
-//     }
-//   };
-
-//   return (
-//     <>
-//       {showRegister === false && (
-//         <div id="authorization">
-//           <div className="container">
-//             <h1>Авторизация</h1>
-//             <div className="authorization">
-//               <div className="authorization__header">
-//                 <p>
-//                   Авторизуйтесь, указав свои контактные данные, или
-//                   воспользовавшись перечисленными сервисами
-//                 </p>
-//               </div>
-//               <div className="authorization__inputs">
-//                 <input
-//                   onInput={validation}
-//                   className="iEmail"
-//                   type="text"
-//                   placeholder="Email или телефон ..."
-//                   value={email}
-//                   onChange={(e) => setEmail(e.target.value)}
-//                 />
-//                 <div className="authorization__inputs--pass">
-//                   <input
-//                     onInput={validation}
-//                     className="iPass"
-//                     type={eye}
-//                     placeholder="Пароль"
-//                     value={password}
-//                     onChange={(e) => setPassword(e.target.value)}
-//                   />
-//                   {eye === "password" ? (
-//                     <FaEyeSlash onClick={handleEye} className="passEye" />
-//                   ) : (
-//                     <FaEye onClick={handleEye} className="passEye" />
-//                   )}
-//                 </div>
-//                 <Link>Забыли пароль?</Link>
-//               </div>
-//               <div className="authorization__button">
-//                 <button onClick={funcs}>Войти</button>
-//               </div>
-//               <div className="authorization__links">
-//                 <h2>Впервые у нас? </h2>
-//                 <Link onClick={() => setShowRegister(true)}>Регистрация</Link>
-//               </div>
-//               <img src={registerImage} alt="" />
-//               <div className="authorization__network">
-//                 <p>Войти через</p>
-//                 <div className="authorization__network-link">
-//                   <AiFillGoogleCircle
-//                     style={{
-//                       width: "33px",
-//                       height: "33px",
-//                     }}
-//                   />
-//                   <FaFacebook
-//                     style={{
-//                       width: "30px",
-//                       height: "30px",
-//                     }}
-//                   />
-//                   <FaTelegram
-//                     style={{
-//                       width: "30px",
-//                       height: "30px",
-//                     }}
-//                   />
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       )}
-//       {showRegister && <Register />}
-//     </>
-//   );
-// };
-
-// export default Authorization;
-
 import "./Authorization.scss";
 import { Link, useNavigate } from "react-router-dom";
 import registerImage from "../../assets/register.png";
@@ -139,117 +6,137 @@ import { FaEye, FaEyeSlash, FaFacebook } from "react-icons/fa";
 import { FaTelegram } from "react-icons/fa";
 import { useState } from "react";
 import Register from "../Register";
-import axios from "axios";
 
 const Authorization = () => {
+  const [active, setActive] = useState(false);
   const [eye, setEye] = useState("password");
+  const [showRegister, setShowRegister] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const [showRegister, setShowRegister] = useState(false);
-
+  const [login, setlogin] = useState(false);
   const nav = useNavigate();
 
+  function Userlogin() {
+    const email = localStorage.getItem("email");
+    if (email) {
+      setlogin(true);
+      nav("/");
+    } else {
+      setlogin(false);
+    }
+  }
+
   const handleEye = () => {
-    setEye((prevEye) => (prevEye === "password" ? "text" : "password"));
+    if (eye === "password") {
+      setEye("text");
+    } else {
+      setEye("password");
+    }
   };
 
-  const loginUser = async () => {
-    if (!email || !password) {
-      setError("Введите email и пароль");
-      return;
+  function iValue() {
+    const iPass = document.querySelector(".iPass");
+    const iEmail = document.querySelector(".iEmail");
+    if (iPass.value.trim() === "") {
+      iPass.style.border = "1px solid red";
+    } else {
+      iPass.style.border = "1px solid #a7c957";
     }
+    if (iEmail.value.trim() === "") {
+      iEmail.style.border = "1px solid red";
+    } else {
+      iEmail.style.border = "1px solid #a7c957";
+    }
+  }
 
-    try {
-      const res = await axios.post(
-        "https://oceanbackend.pythonanywhere.com/user/",
-        {
-          email,
-          password,
-        }
-      );
-
-      if (res.status === 200) {
-        nav("/"); // Перенаправление на главную страницу
-      } else {
-        setError("Неверный email или пароль");
-      }
-    } catch (error) {
-      setError("Произошла ошибка. Попробуйте еще раз.");
+  function funcs() {
+    btnActive();
+    Userlogin();
+  }
+  const btnActive = () => {
+    setActive(true);
+    iValue();
+  };
+  const validation = () => {
+    if (active === true) {
+      iValue();
     }
   };
 
   return (
     <>
-      <div id="authorization">
-        <div className="container">
-          <h1>Авторизация</h1>
-          <div className="authorization">
-            {error && <p className="error-message">{error}</p>}
-            <div className="authorization__header">
-              <p>
-                Авторизуйтесь, указав свои контактные данные, или
-                воспользовавшись перечисленными сервисами
-              </p>
-            </div>
-            <div className="authorization__inputs">
-              <input
-                className="iEmail"
-                type="text"
-                placeholder="Email или телефон ..."
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <div className="authorization__inputs--pass">
-                <input
-                  className="iPass"
-                  type={eye}
-                  placeholder="Пароль"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                {eye === "password" ? (
-                  <FaEyeSlash onClick={handleEye} className="passEye" />
-                ) : (
-                  <FaEye onClick={handleEye} className="passEye" />
-                )}
+      {showRegister === false && (
+        <div id="authorization">
+          <div className="container">
+            <h1>Авторизация</h1>
+            <div className="authorization">
+              <div className="authorization__header">
+                <p>
+                  Авторизуйтесь, указав свои контактные данные, или
+                  воспользовавшись перечисленными сервисами
+                </p>
               </div>
-              <Link>Забыли пароль?</Link>
-            </div>
-            <div className="authorization__button">
-              <button onClick={loginUser}>Войти</button>
-            </div>
-            <div className="authorization__links">
-              <h2>Впервые у нас? </h2>
-              <Link onClick={() => setShowRegister(true)}>Регистрация</Link>
-            </div>
-            <img src={registerImage} alt="" />
-            <div className="authorization__network">
-              <p>Войти через</p>
-              <div className="authorization__network-link">
-                <AiFillGoogleCircle
-                  style={{
-                    width: "33px",
-                    height: "33px",
-                  }}
+              <div className="authorization__inputs">
+                <input
+                  onInput={validation}
+                  className="iEmail"
+                  type="text"
+                  placeholder="Email или телефон ..."
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
-                <FaFacebook
-                  style={{
-                    width: "30px",
-                    height: "30px",
-                  }}
-                />
-                <FaTelegram
-                  style={{
-                    width: "30px",
-                    height: "30px",
-                  }}
-                />
+                <div className="authorization__inputs--pass">
+                  <input
+                    onInput={validation}
+                    className="iPass"
+                    type={eye}
+                    placeholder="Пароль"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  {eye === "password" ? (
+                    <FaEyeSlash onClick={handleEye} className="passEye" />
+                  ) : (
+                    <FaEye onClick={handleEye} className="passEye" />
+                  )}
+                </div>
+                <Link>Забыли пароль?</Link>
+              </div>
+              <div className="authorization__button">
+                <button onClick={funcs}>Войти</button>
+              </div>
+              <div className="authorization__links">
+                <h2>Впервые у нас? </h2>
+                <Link onClick={() => setShowRegister(true)}>Регистрация</Link>
+              </div>
+              <img src={registerImage} alt="" />
+              <div className="authorization__network">
+                <p>Войти через</p>
+                <div className="authorization__network-link">
+                  <AiFillGoogleCircle
+                    style={{
+                      width: "33px",
+                      height: "33px",
+                    }}
+                  />
+                  <FaFacebook
+                    style={{
+                      width: "30px",
+                      height: "30px",
+                    }}
+                  />
+                  <FaTelegram
+                    style={{
+                      width: "30px",
+                      height: "30px",
+                    }}
+                  />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
       {showRegister && <Register />}
     </>
   );
